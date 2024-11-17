d297d61bf03eecdd6a26e1712171d240df56ab3a
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2024-11-17 16:08:46 +0700

    update lab2 ex6-ex9

---
+++ labwork2/ex7.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // Change the href attribute
+    $("#usth").attr("href", "http://www.newlink.com/");
+    // Change the text of the link
+    $("#usth").text("New Link Text");
+});
